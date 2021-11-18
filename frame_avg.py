6aa52c41a9a01954f33bc60ab27f71c9b4048987
SimeonBruylandOqton <simeon.bruyland@oqton.com>
2021-11-18 19:43:36 +0100

    Merge delete files

--- frame_avg.py
+++
@@ -1,8 +0,0 @@
-from utils_feature_preprocessing import split_features_into_frames
-
-def extract_features(pose_sequence):
-    # You should implement this function to return better features!
-    
-    pose_sequence = split_features_into_frames(pose_sequence, k=2)
-    
-    return pose_sequence

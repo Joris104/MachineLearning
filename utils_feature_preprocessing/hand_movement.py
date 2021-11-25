d183d19792e10bfb6a84c243c788133bfba6f2b8
Joris Vandenbroeck <Joris.Vandenbroeck@Ugent.be>
2021-11-25 14:09:56 +0100

    Total movement feature

---
+++ utils_feature_preprocessing/hand_movement.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def hand_movement(keypoints):
+    lhand = keypoints[:,83:104,:]
+    rhand = keypoints[:,104:,:]
+    #print(rhand)
+    # for kp in keypoints[1:]:
+        # prev_lhand = lhand
+        # prev_rhand = rhand
+        # lhand = kp[83:104,:]
+        # rhand = kp[104:,:]
+        # lhand_mov = np.abs(lhand-prev_lhand)+lhand_mov
+        # rhand_mov = np.abs(rhand-prev_rhand)+rhand_mov
+    
+    lhand_mov = np.abs(np.diff(lhand, axis=0))
+    rhand_mov = np.abs(np.diff(rhand, axis=0))
+    #print(lhand_mov)
+    lhand_mov = np.sum(lhand_mov, axis=0)
+    lhand_mov = np.sum(lhand_mov, axis=0)
+    rhand_mov = np.sum(rhand_mov, axis=0)
+    #print(rhand_mov)
+    rhand_mov = np.sum(rhand_mov, axis=0)
+    #print(rhand_mov)
+    return lhand_mov[:2], rhand_mov[:2]

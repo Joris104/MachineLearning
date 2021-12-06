d211d088f3e7d3c9dffb0ff51ab1f46ccf716393
mdagrain <Miguel.Dagrain@UGent.be>
2021-12-06 19:54:52 +0100

    Remove start same position

--- utils_feature_preprocessing/start_same_position.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def translate_body(pose_sequence):
-    #translates pose sequence so that middle between shoulders starts at the same position in the first frame
-    center_between_shoulders = (pose_sequence[0][11] + pose_sequence[0][12])/2
-    return pose_sequence - center_between_shoulders

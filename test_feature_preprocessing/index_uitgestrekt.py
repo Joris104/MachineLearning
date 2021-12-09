13892aaadc257d3b3ce102e83ea6bd82b8c1cd49
Joris Vandenbroeck <Joris.Vandenbroeck@Ugent.be>
2021-12-09 10:26:34 +0100

    Ensemble hypertuning

--- test_feature_preprocessing/index_uitgestrekt.py
+++ test_feature_preprocessing/index_uitgestrekt.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def index_uitgestrekt(poses):
-    thumb = np.array([1, 2, 3, 4])
-    index = np.array([5, 6, 7, 8])
-    middle = np.array([ 9, 10, 11, 12])
-    ring = np.array([13, 14, 15, 16])
-    pinky = np.array([17, 18, 19, 20])
+    thumb = np.array([0,1, 2, 3, 4])
+    index = np.array([0,5, 6, 7, 8])
+    middle = np.array([0, 9, 10, 11, 12])
+    ring = np.array([0,13, 14, 15, 16])
+    pinky = np.array([0,17, 18, 19, 20])
     
     fingers = np.vstack((thumb, index, middle, ring, pinky))
     features = np.empty(len(poses), dtype=np.object)

dc85aa364356eb9bf4d67d2c55e62fefaffc994b
mdagrain <Miguel.Dagrain@UGent.be>
2021-12-10 01:35:32 +0100

    usefull changes to feature hands

--- utils_feature_preprocessing/pose_hands.py
+++ utils_feature_preprocessing/pose_hands.py
@@ -27,25 +27,25 @@ def pose_hand(pose_sequence):
     right_hands =  rotate_seq(right_hands, 0, 1)    
     
     #take the directions
-    left = transform_to_k_frames(left_hands, k=1)
-    right = transform_to_k_frames(right_hands, k=1)
+    left1 = transform_to_k_frames(left_hands, k=1)
+    right1 = transform_to_k_frames(right_hands, k=1)
+    left = transform_to_k_frames(left_hands, k=2)
+    right = transform_to_k_frames(right_hands, k=2)
+
+
     connections = []
     for connection in HAND_CONNECTIONS:
+        connections.append(left1[0, connection[0]] - left1[0,connection[1]])
+        connections.append(right1[0, connection[0]] - right1[0,connection[1]])
         connections.append(left[0, connection[0]] - left[0,connection[1]])
         connections.append(right[0, connection[0]] - right[0,connection[1]])
-
-    #also take first couple of frames to average and last couple of frames
-    left_begin = select_frames(left_hands, 0, 3) # 0 1 2
-    left_end = select_frames(left_hands, -1, -3) # -3 -2 -1
-    right_begin = select_frames(right_hands, 0, 3)
-    right_end = select_frames(right_hands, -1, -3)
+        connections.append(left[1, connection[0]] - left[1,connection[1]])
+        connections.append(right[1, connection[0]] - right[1,connection[1]])
 
     #we now look at how the hands have moved throughout the video:
-    mov_left = left_end-left_begin
-    mov_right = right_end-right_begin
+   # mov_left = left_end-left_begin
+   # mov_right = right_end-right_begin
 
     connections = frames_to_features(connections)
-    mov_left = frames_to_features(mov_left)
-    mov_right = frames_to_features(mov_right)
 
     return connections#np.concatenate((connections, mov_left, mov_right))
